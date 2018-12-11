e3b9be41738cab08df97213f3f5d3072fd832e67
pgerspach <pgerspac@alumni.nd.edu>
2018-12-10 20:32:01 -0500

    Added liri

--- assets/javascript/appAbout.js
+++ assets/javascript/appAbout.js
@@ -1,4 +1,14 @@
 $(document).ready(() => {
+  var config = {
+    authDomain: "portfolio-contact-10952.firebaseapp.com",
+    databaseURL: "https://portfolio-contact-10952.firebaseio.com",
+    projectId: "portfolio-contact-10952",
+    storageBucket: "portfolio-contact-10952.appspot.com",
+    messagingSenderId: "134741402089"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
   showAnimation("welcomeSwipe", "6", "canChangeW");
   function showAnimation(anim, duration, whichClass) {
     $(`.${whichClass}`).attr(
@@ -7,5 +17,21 @@ $(document).ready(() => {
     );
   }
 
+  $(".contactSubmit").on("click", (event)=>{
+    event.preventDefault();
+    let message = $(".form-message").val();
+    let email = $(".form-email").val();
+    let name = $(".form-inp").val();
+    database.ref('messages').push({
+      message:message,
+      email:email,
+      name:name
+    });
+    $(".form-message").val("");
+    $(".form-email").val("");
+    $(".form-inp").val("");
+
+
+  })
 
 });
